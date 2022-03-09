f51995ff88d490c69762790bb08656de98c2b85a
GabriellCastro <kastro.gabriel17@gmail.com>
2022-03-09 01:11:03 -0400

    add: usando controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
+/* eslint-disable import/extensions */
 import express from 'express';
 import bodyParser from 'body-parser';
+// eslint-disable-next-line import/no-unresolved
+import { providerController } from './controllers/providerController';
 
 const app: express.Application = express();
 
 app.use(bodyParser.json());
 
-app.use('/providers');
+app.use('/providers', providerController);
 
 const PORT = process.env.PORT || 3000;
 
